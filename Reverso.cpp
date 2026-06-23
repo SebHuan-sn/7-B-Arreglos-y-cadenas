b28600044cb305ffe655c4409a81bef96b9fb6d4
SebHuan-ns <ashuancam@unjbg.edu.pe>
2026-06-22 23:23:40 -0500

    Ejercicio1: actualiza a salidas por lineas y validacion de rango

--- Reverso.cpp
+++ Reverso.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
+
 int main() {
   int n;
-  cin>>n;
+  cin >> n;
+
+  // Añadimos el tamaño para que no rompa el arreglo estático de 1002
+  if (n <= 0 || n > 1000) {
+    cout << "Tamaño no valido" << endl;
+    return 1; 
+  }
+
   int devol[1002];
   for(int i=0; i<n; i++){
-    cin>>devol[i];
+    cin >> devol[i];
   }
+
+  // Imprimir en reverso con saltos de línea claros
   for(int i=n-1; i>=0; i--){
-    cout<<devol[i]<<" ";
+    cout << devol[i] << "\n";
   }
+
   return 0;
 }
