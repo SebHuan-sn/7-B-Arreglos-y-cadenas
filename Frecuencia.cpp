46ddc063f351ad4a80fb6d5b58af6111b52ac897
SebHuan-ns <ashuancam@unjbg.edu.pe>
2026-06-24 00:48:09 -0500

    Ejercicio3: Version interactiva de frecuencias con mensajes de guia

--- Frecuencia.cpp
+++ Frecuencia.cpp
@@ -2,17 +2,23 @@
 
 using namespace std;
 int main(){
-    int p;
-    int n;
-    cin>>p>>n;
+    int p, n;
+    cout<<"Ingrese la cantidad de profesores: "<<endl;
+    cin>>p;
+    cout<<"Ingrese la cantidad de alumnos: "<<endl;
+    cin>>n;
     int votos[102]={0};
+    cout<<"Ingrese los "<<n<<"votos de los alumnos"<<endl;
     for(int i=0; i<n; i++){
         int votoprim;
+        cout<<"Voto del alumno "<<(i+1)<<": ";
         cin>>votoprim;
         votos[votoprim]++;
     }
+    cout << "----------------------------------------" << endl;
+    cout << "TABLA DE FRECUENCIAS (Resultados):" << endl;
     for(int i=1; i<=p; i++){
-        cout<<i<<"-"<<votos[i]<<endl;
+        cout<<"Profesor nro "<<i<<"->"<<votos[i]<<" votos"<<endl;
     }
     return 0;
 }
