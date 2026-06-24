d11f5e6f380e12eccf844ed101041dc47db01af7
SebHuan-ns <ashuancam@unjbg.edu.pe>
2026-06-23 23:23:58 -0500

    Ejercicio2: Version interactiva de palindromos con mensajes de guia

--- palindromo.cpp
+++ palindromo.cpp
@@ -1,31 +1,35 @@
-#include<iostream>
-using namespace std;
+    #include<iostream>
+    using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    for(int i=0; i<n; i++){
-        char palabra[105];
-        cin>>palabra;
+    int main(){
+        int n;
+        cout << "Ingrese la cantidad de palabras a evaluar: ";
+        cin >> n;
+        cout << "----------------------------------------" << endl;
+        for(int i=0; i<n; i++){
+            cout << "Ingrese la palabra #" << (i + 1) << ": ";
+            char palabra[105];
+            cin>>palabra;
 
-        int t=0;
-        while(palabra[t]!='\0'){
-            t++;
-        }
-        
-        bool espalin=true;
-        for(int j=0; j<t/2; j++){
-            if(palabra[j]!=palabra[t-1-j]){
-                espalin=false;
-                break;
+            int t=0;
+            while(palabra[t]!='\0'){
+                t++;
+            }
+            
+            bool espalin=true;
+            for(int j=0; j<t/2; j++){
+                if(palabra[j]!=palabra[t-1-j]){
+                    espalin=false;
+                    break;
+                }
+            }
+            cout << "Resultado: "; 
+            if (espalin==true){
+                cout<<"P"<<endl;
+            }
+            else{
+                cout<<"NP"<<endl;
             }
         }
-        if (espalin==true){
-            cout<<"P"<<endl;
-        }
-        else{
-            cout<<"NP"<<endl;
-        }
+        return 0;
     }
-    return 0;
-}
