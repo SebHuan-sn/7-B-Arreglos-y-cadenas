48e1fbf1ddec3e5a267dceaf9576a89cd28e5732
SebHuan-ns <ashuancam@unjbg.edu.pe>
2026-06-24 08:34:33 -0500

    Ejercicio4: Version mejorada de canicas

--- canicas.cpp
+++ canicas.cpp
@@ -2,25 +2,37 @@
 
 using namespace std;
 
-// Creamos el arreglo AQUÍ AFUERA (global). 
-// Lo hacemos de 1000005 para asegurarnos de que quepan los 1,000,000 de vasos.
-// Al declararlo aquí afuera, C++ automáticamente lo llena de CEROS.
-int contavasos[1000005];
+int contavasos[1000010];
 
 int main() {
     int vasos, canicas;
-    cin>>vasos>>canicas;
+    
+    // Solicitamos los datos principales con estilo
+    cout << "========================================" << endl;
+    cout << "            CONTEO DE CANICAS           " << endl;
+    cout << "========================================" << endl;
+    cout << "Ingrese la cantidad de vasos (N): ";
+    cin >> vasos;
+    cout << "Ingrese la cantidad de canicas a lanzar (C): ";
+    cin >> canicas;
+    cout << "----------------------------------------" << endl;
 
+    cout << "Por favor, introduzca el numero de vaso donde cayo cada canica:" << endl;
     for (int i = 0; i < canicas; i++) {
         int vasocayo;
-        cin>>vasocayo;
-        
+        cout << "Canica #" << (i + 1) << " cayo en el vaso: ";
+        cin >> vasocayo;
         contavasos[vasocayo]++;
     }
 
+    cout << "----------------------------------------" << endl;
+    cout << "RESULTADOS FINALES (Canicas por vaso):" << endl;
+    cout << "----------------------------------------" << endl;
+
     for (int i = 1; i <= vasos; i++) {
-        cout<<contavasos[i] << "\n";
+        cout << "Vaso " << i << " contiene: " << contavasos[i] << " canicas" << endl;
     }
+    cout << "========================================" << endl;
 
     return 0;
 }
